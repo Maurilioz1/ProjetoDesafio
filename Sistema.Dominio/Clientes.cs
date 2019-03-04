d466fc576f2676a04ec40c75589d72b2a5cbd962
Maurilio <maurilioz1@hotmail.com>
2019-03-04 15:41:57 -0300

    Correção de bugs

--- Sistema.Dominio/Clientes.cs
+++ Sistema.Dominio/Clientes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,32 @@ namespace Sistema.Dominio
     public class Clientes
     {
         public int id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite a razão social.")]
         public string razaoSocial { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o nome fantasia.")]
         public string nomeFantasia { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o CNPJ.")]
         public string cnpj { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o endereço.")]
         public string endereço { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o número.")]
         public int numero { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o bairro.")]
         public string bairro { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o complemento.")]
         public string complemento { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o município.")]
         public string municipio { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite o CEP.")]
         public string cep { get; set; }
     }
 }
--- Sistema.Dominio/Usuarios.cs
+++ Sistema.Dominio/Usuarios.cs
@@ -1,11 +1,21 @@
-﻿namespace Sistema.Dominio
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Dominio
 {
     public class Usuarios
     {
         public int id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite seu nome completo.")]
         public string nomeCompleto { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite um apelido.")]
         public string apelido { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite um email.")]
         public string email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Digite um telefone.")]
         public string telefone { get; set; }
     }
 }
